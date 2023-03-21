cf71fb0e1fd5770e141f681803aa5bb32da27a93
luizfiller <luiz.fsilva@sptech.school>
2023-03-21 12:08:31 -0300

    Update - refactoring map and catchError of Observable search

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
-import { Item, LivrosResultado } from './../models/interfaces';
+import { LivrosResultado } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,15 +11,10 @@ export class LivroService {
 
   constructor(private http: HttpClient) {}
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
 
     // Pipe -> agrupar diversos tipos de operadores | Cano que passa o fluxo de informações e é onde serão aplicadas as transformações de dados
-    return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      // Tap -> é como se você um espião, utilizado especificamente para Debug | Ele não modifica os dados, apenas ajuda a visualizá-los
-      // tap((retornoAPI) => console.log('Fluxo do tap: ',retornoAPI)),
-      map(resultado => resultado.items),
-      // tap(resultado => console.log('Fluxo após o map: ', resultado))
-    )
+    return this.http.get<LivrosResultado>(this.API, { params });
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,18 @@
-import { Item } from './../../models/interfaces';
+import { Item, LivrosResultado } from './../../models/interfaces';
 import { LivroService } from './../../service/livro.service';
 import { Component } from '@angular/core';
-import { switchMap, map, tap, filter, debounceTime, distinctUntilChanged, catchError, throwError, EMPTY } from 'rxjs';
+import {
+  switchMap,
+  map,
+  tap,
+  filter,
+  debounceTime,
+  distinctUntilChanged,
+  catchError,
+  throwError,
+  EMPTY,
+  of,
+} from 'rxjs';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { FormControl } from '@angular/forms';
 
@@ -10,15 +21,16 @@ const PAUSA = 300;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent{
-
+export class ListaLivrosComponent {
   // FormControl é possível ter acesso a valores, status de validação e interações do usuário e eventos
   campoBusca = new FormControl();
   mensagemErro = '';
+  livrosResultado: LivrosResultado;
+  listaLivros: LivroVolumeInfo[];
 
-  constructor(private service: LivroService) { }
+  constructor(private service: LivroService) {}
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     // O 'debounceTime' serve para dar um delay à busca para que o usuário consiga preencher o input e logo começar a buscar pela string inteira
@@ -26,28 +38,36 @@ export class ListaLivrosComponent{
 
     // O 'filter' recebe uma condição, a qual, se for satisfeita por um dado inserido/recebido, o fluxo de comandos segue.
     filter((valorDigitado) => valorDigitado.length >= 3),
-    tap(() => console.log('Fluxo Inicial: ')),
 
     // O 'distinctUntilChanged' serve para comparar valores recebidos e a requisição só é feita quando os valores são diferentes
     distinctUntilChanged(),
 
     // O 'switchMap' utiliza apenas o último valor digitado para fazer a requisição, ele desconsidera os valores anteriormente inputados
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    tap(() => console.log('Requisição ao servidor: ')),
-    map(items => this.livrosResultadoParaLivros(items)),
-    catchError(erro => {
-      this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.'
-      return EMPTY;
+    map((resultado) => (this.livrosResultado = resultado)),
+    tap((retornoAPI) => console.log(retornoAPI)),
+    map((resultado) => resultado.items ?? []),
+    map((items) => this.livrosResultadoParaLivros(items)),
+    catchError((erro) => {
+      // this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.'
+      // return EMPTY;
 
       // Ao invez do bloco de código acima, pode ser utilizado o 'throwError' para emitir imediatamente a mensagem de erro e, após isso, ele encerrar seu ciclo de vida
-      // return throwError(() => new Error(this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.'))
+      console.log(erro);
+      return throwError(
+        () =>
+          new Error(
+            (this.mensagemErro =
+              'Ops, ocorreu um erro. Recarregue a aplicação.')
+          )
+      );
     })
-    )
+  );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
-    return items.map(item => {
-      return new LivroVolumeInfo(item)
-    })
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
   }
 
   // O ngOnDestroy serve para fazer o unsubscribe, porém o pipe async ( | async ) já realiza isso quando inserido no template HTML
